fd7c7ad155745cbc3a033ba99e2063bf1a17f80e
rikulehtonen <mail@rikulehtonen.fi>
2025-12-13 23:51:35 +0200

    fix communication cycle

--- demo/ESP32-demo/ESP32-demo.ino
+++ demo/ESP32-demo/ESP32-demo.ino
@@ -30,9 +30,19 @@ File myFile;
 void setup() {
     // Ensure all CS pins are HIGH before SPI begin
 
+    UDOUBLE Width, Height;
+    UBYTE Color;
+    Width = (EPD_13IN3E_WIDTH % 2 == 0)? (EPD_13IN3E_WIDTH / 2 ): (EPD_13IN3E_WIDTH / 2 + 1);
+    Height = EPD_13IN3E_HEIGHT;
+    Color = (EPD_13IN3E_WHITE<<4)|EPD_13IN3E_WHITE;
+        
+    UBYTE buf[Width/2];
+    for (UDOUBLE j = 0; j < Width/2; j++) {
+        buf[j] = Color;
+    }
+
     Debug("EPD_13IN3E_test Demo\r\n");
     DEV_Module_Init();
-
 
     SPI.begin(sck, miso, mosi);
     if (!SD.begin(cs)) {
@@ -44,35 +54,53 @@ void setup() {
     String nextFile = getNextFile();
     Serial.printf("Processing file: %s\n", nextFile.c_str());
 
+    UDOUBLE n = 0;
     // Read index from config.txt
     File myFile = SD.open("/" + nextFile, FILE_READ);
-    if (myFile) {
-        String index_string = "";
-        while (myFile.available()) {
-            index_string += (char)myFile.read();
+    UDOUBLE j = 0;
 
-            Debug(index_string);
-
-            Debug("EPD_13IN3E_test Demo\r\n");
-            DEV_Module_Init();
-            Debug("e-Paper Init...\r\n");
-            EPD_13IN3E_Init();
-
-            pinMode(cs, OUTPUT);
-            pinMode(eink_cs_m, OUTPUT);
-            pinMode(eink_cs_s, OUTPUT);
-            digitalWrite(cs, HIGH);
-            digitalWrite(eink_cs_m, LOW);
-            digitalWrite(eink_cs_s, LOW);
-            EPD_13IN3E_Clear(EPD_13IN3E_WHITE);
-
+    if(myFile.available()) {
+        for (UDOUBLE j = 0; j < Width/2; j++) {
+            buf[j] = myFile.read();
         }
-        myFile.close();
+            
+        // Print the buffer contents
+        Serial.print("Buffer contents: ");
+        for (UDOUBLE j = 0; j < Width/2; j++) {
+            Serial.printf("0x%02X ", buf[j]);
+        }
+        Serial.println();
     } else {
-        Serial.println("Error opening config.txt for reading");
+        Serial.println("FAILED");
     }
 
 
+    //Debug("EPD_13IN3E_test Demo\r\n");
+    DEV_Module_Init();
+    //Debug("e-Paper Init...\r\n");
+    EPD_13IN3E_Init();
+    pinMode(cs, OUTPUT);
+    pinMode(eink_cs_m, OUTPUT);
+    pinMode(eink_cs_s, OUTPUT);
+    digitalWrite(cs, HIGH);
+    digitalWrite(eink_cs_m, LOW);
+    digitalWrite(eink_cs_s, LOW);
+    EPD_13IN3E_Clear(EPD_13IN3E_WHITE);
+
+
+    if(myFile.available()) {
+        for (UDOUBLE j = 0; j < Width/2; j++) {
+            buf[j] = myFile.read();
+        }  
+        // Print the buffer contents
+        Serial.print("Buffer contents: ");
+        for (UDOUBLE j = 0; j < Width/2; j++) {
+            Serial.printf("0x%02X ", buf[j]);
+        }
+        Serial.println();
+    } else {
+        Serial.println("FAILED");
+    }
 
     Debug("Goto Sleep...\r\n");
     EPD_13IN3E_Sleep();
@@ -80,6 +108,9 @@ void setup() {
     Debug("close 5V, Module enters 0 power consumption ...\r\n");
     DEV_Module_Exit();
 }
+
+
+
 
 String getNextFile() {
     int config_index = 0;
